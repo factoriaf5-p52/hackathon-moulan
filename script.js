1026bffb8bcd35103617d6a1e7b088ecd06db993
Sablinita <sablinahare@gmail.com>
2022-12-11 13:57:00 +0100

    done another revision

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-//Reto 4
-const audio = document.getElementById('reproductor');
+//Reto 1
 
-audio.addEventListener("pause", () => {
-
-    alert("Se ha pausado la reproducción");
-
-});
-
-//Reto 3
-
-const formulario = document.getElementById('contacte');
-
-formulario.addEventListener("submit", () => {
-
-    alert("Se ha enviado el formulario");
-
-});
+const play= () =>{
+    const target = document.querySelector('.target');
+    
+    if(target != undefined){
+        target.className = '';
+    }
+    
+    const circulo = document.getElementById(Math.floor(Math.random()*9+1))
+    circulo.className = 'target';
+    
+    circulo.addEventListener('click', () =>{
+        clearTimeout(id);
+        alert('you win')
+    })
+    const id = setTimeout(play, 3000);
+    }
 
 //Reto 2
 
@@ -38,3 +38,72 @@ cuadrado.addEventListener("keydown", (evento) => {
     }
 
 });
+
+//Otra respuesta alternativa al Reto 2:
+//const tests = document.getElementById("tests");
+
+//tests.addEventListener("keydown", (k)=>{
+   // if(k.key=="r"){
+      //  tests.className="red"
+   // }else if(k.key=="y"){
+     //   tests.className=="yellow"
+   // }else if(k.key=="w"){
+     //   tests.className="white";
+   // }
+//})
+
+//Reto 3
+
+const formulario = document.getElementById('contacte');
+
+formulario.addEventListener("submit", () => {
+
+    alert("Se ha enviado el formulario");
+
+});
+
+// Otra solución
+
+// function validation(x){
+//     alert("validation here")
+    
+//     }
+    
+//     document.querySelector('#formbutton').addEventListener("click", validation);
+    
+
+//Reto 4
+const audio = document.getElementById('reproductor');
+
+//Respuesta alternativa al Reto 4:
+//let audio = document.getElementById('reproductor');
+//audio.onpause = function(){
+    //alert('audio pausado');
+//}
+
+audio.addEventListener("pause", () => {
+
+    alert("Se ha pausado la reproducción");
+
+});
+
+//Reto 5
+
+const addButton = document.getElementById('addButton');
+const keywordElement = document.getElementById('keyword');
+const valElement = document.getElementById('value');
+
+
+addButton.addEventListener('click',function(){
+    localStorage.setItem(keywordElement.value, valElement.value);
+    alert('se ha modificado el localstorage desde la página principal')
+});
+
+//window.addEventListener('storage',()=>alert(se ha modificado el localstorage'))
+
+
+
+
+
+
+
